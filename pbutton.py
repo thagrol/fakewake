c0a5a1fadf28a4faa49aa55baff7764e383216fc
Simon Osborne <thagrol@btinternet.com>
2016-07-05 16:22:11 +0000

    Poweroff pi on button press

---
+++ pbutton.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+## simple script to monitor a gpio pin with attached button
+## and shutdown pi when pressed for > 2 seconds
+## must be run as root or via sudo
+
+# imports
+from gpiozero import Button
+from os import system
+from time import sleep
+
+# config
+PIN = 17
+PULL_UP = True
+HOLD_TIME = 2
+
+# functions
+def btn_held():
+    # respond to long press on button
+    os.system("poweroff")
+
+# do stuff
+if __name__ == '__main__':
+    pbutton = Button(pin=PIN,
+                     pull_up=PULL_UP,
+                     hold_time=HOLD_TIME,
+                     hold_repeat=False)
+    pbutton.when_held = btn_held
+
+    while True:
+       sleep(1)
